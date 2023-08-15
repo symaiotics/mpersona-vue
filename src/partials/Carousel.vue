7bf51e94baffeba7edb9687ebb208c64066ebba2
Janak ALford <info@symaiotics.com>
2023-08-14 20:29:23 -0400

    Initial commit

---
+++ src/partials/Carousel.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="border-t border-transparent dark:border-gray-800">
+    <div class="py-12 md:py-20">
+
+      <div class="max-w-6xl mx-auto px-4 sm:px-6">
+
+        <!-- Section header -->
+        <div class="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+          <h1 class="h2 font-red-hat-display mb-4">Personas drive Large Language Model (LLM) Interactions</h1>
+          <p class="text-xl text-gray-600 dark:text-gray-400">Leverage sophisticated community-built personas to support your interactions.</p>
+          <br/>
+          <p class="text-xl text-gray-600 dark:text-gray-400">Best of all this is 100% open source. </p>
+          <p class="text-xl text-gray-600 dark:text-gray-400"> Deploy to your own Cloud or to your secure data centre.</p>
+        </div>
+
+      </div>
+ 
+
+    </div>
+  </section>
+</template>
+
+<script>
+// Import Swiper
+import Swiper, { Autoplay, Navigation } from 'swiper'
+import 'swiper/css'
+Swiper.use([Autoplay, Navigation])
+
+export default {
+  name: 'Carousel',
+  mounted() {
+    // eslint-disable-next-line no-unused-vars
+    const carousel = new Swiper('.carousel', {
+      slidesPerView: 'auto',
+      grabCursor: true,
+      loop: true,
+      centeredSlides: true,
+      initialSlide: 1,
+      spaceBetween: 24,
+      autoplay: {
+        delay: 7000,
+      },
+      navigation: {
+        nextEl: '.carousel-next',
+        prevEl: '.carousel-prev',
+      },
+    })
+  }
+}
+</script>
---
+++ src/partials/HeroHome.vue
@@ -0,0 +1,58 @@
+<template>
+  <section>
+    <div class="max-w-6xl mx-auto px-4 sm:px-6">
+      <div class="pt-32 pb-10 md:pt-40 md:pb-16">
+
+        <!-- Hero content -->
+        <div class="md:grid md:grid-cols-12 md:gap-12  items-center">
+
+          <!-- Content -->
+          <div class="md:col-span-12 lg:col-span-12 mb-8 md:mb-0 text-center  ">
+
+            <img class="relative max-w-full mx-auto h-auto pointer-events-none" style = "width:300px" src="/images/splash1.png" alt="iPhone mockup" aria-hidden="true" />
+
+
+            <h1 class="h1 lg:text-6xl mb-4 font-red-hat-display font-black" data-aos="fade-down">mPersona.com</h1>
+            <p class="text-xl text-gray-600 dark:text-gray-400" data-aos="fade-down" data-aos-delay="150">Unlock the power of persona-driven AI interactions.</p>
+ 
+          </div>
+
+          <!-- Mobile mockup -->
+          <!-- <ModalVideoIphone
+            :thumb="Splash1"
+            :thumbWidth="290"
+            :thumbHeight="624"
+            thumbAlt="Modal video thumbnail"
+            :video="VideoSrc"
+            :videoWidth="1920"
+            :videoHeight="1080" /> -->
+
+      
+
+
+        </div>
+        
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import ModalVideoIphone from './ModalVideoIphone.vue'
+import Splash1 from '../images/splash1.png'
+import VideoThumb from '../images/mockup-image-01.jpg'
+import VideoSrc from '../videos/video.mp4'
+
+export default {
+  name: 'HeroHome',
+  components: {
+    ModalVideoIphone
+  },
+  setup() {
+    return {
+      VideoThumb,
+      VideoSrc,
+    }
+  }  
+}
+</script>
