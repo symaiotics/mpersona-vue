c1e6b4665b186bd10821726c4e4a07c33d21b8e9
Janak ALford <info@symaiotics.com>
2024-01-06 07:11:47 -0500

    Support VITE and non VITE vars

--- src/env.js
+++ src/env.js
@@ -1,9 +1,11 @@
 // src/config/env.js
 const env = {
-    SELF: import.meta.env.SELF ||  "beeep",
-    API_URL: import.meta.env.API_URL ||  "bloop",
-    STORAGE_URL: import.meta.env.STORAGE_URL  || "bloorp",
-    WEBSOCKET_URL: import.meta.env.WEBSOCKET_URL || "blump",
-  };
-  
-  export default env;
+    SELF:  import.meta.env.SELF ||import.meta.env.VITE_SELF || "beeep",
+    API_URL:  import.meta.env.API_URL || import.meta.env.VITE_API_URL || "bloop",
+    STORAGE_URL: import.meta.env.STORAGE_URL || import.meta.env.VITE_STORAGE_URL ||  "bloorp",
+    WEBSOCKET_URL: import.meta.env.WEBSOCKET_URL || import.meta.env.VITE_WEBSOCKET_URL || "blump",
+};
+
+console.log(import.meta.env)
+
+export default env;
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import ImportMetaEnvPlugin from "@import-meta-env/unplugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+ 
   plugins: [
     vue(),
     ImportMetaEnvPlugin.vite({
