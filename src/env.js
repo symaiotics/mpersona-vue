f045f4e890b73c9de13dca7065bc232813d80681
Janak ALford <info@symaiotics.com>
2024-01-06 22:10:44 -0500

    Updated vite and .env

--- src/env.js
+++ src/env.js
@@ -1,11 +1,11 @@
-// src/config/env.js
+//env.js loads in the environment variables
 const env = {
-    SELF:  import.meta.env.SELF ||import.meta.env.VITE_SELF || "beeep",
-    API_URL:  import.meta.env.API_URL || import.meta.env.VITE_API_URL || "bloop",
-    STORAGE_URL: import.meta.env.STORAGE_URL || import.meta.env.VITE_STORAGE_URL ||  "bloorp",
-    WEBSOCKET_URL: import.meta.env.WEBSOCKET_URL || import.meta.env.VITE_WEBSOCKET_URL || "blump",
+    SELF: import.meta.env.SELF || import.meta.env.VITE_SELF || "MISSING ENV URL",
+    API_URL: import.meta.env.API_URL || import.meta.env.VITE_API_URL || "MISSING ENV URL",
+    STORAGE_URL: import.meta.env.STORAGE_URL || import.meta.env.VITE_STORAGE_URL || "MISSING ENV URL",
+    WEBSOCKET_URL: import.meta.env.WEBSOCKET_URL || import.meta.env.VITE_WEBSOCKET_URL || "MISSING ENV URL",
 };
 
-console.log(import.meta.env)
+console.log('Booting with full .env', import.meta.env)
 
 export default env;
--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,31 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import ImportMetaEnvPlugin from "@import-meta-env/unplugin";
-
-// https://vitejs.dev/config/
-export default defineConfig({
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import ImportMetaEnvPlugin from '@import-meta-env/unplugin'
  
+export default defineConfig({
+  server: {
+    cors: true,
+    strictPort: true,
+    headers: {
+      'Cache-Control': 'no-cache',
+      'X-Content-Type-Options': 'nosniff',
+      'Strict-Transport-Security':
+        'max-age=31536000; includeSubDomains; preload',
+      'Referrer-Policy': 'strict-origin-when-cross-origin'
+    }
+  },
+
   plugins: [
     vue(),
     ImportMetaEnvPlugin.vite({
-      example: ".env.example",
-      // "env": "...",
-      // "transformMode": "..."
-    }),
+      example: '.env'
+    })
+
   ],
+
   resolve: {
-    alias: {
-      '@': '/src',
-    },
+    alias: [{ find: '@', replacement: '/src' }],
+    dedupe: ['vue']
   },
-  // Additional Vite configuration...
-});
+})
